4114c85ab2fa90c54c3e460e65ab8bf36938c676
Thomaz Gomes <thomazscg@gmail.com>
2025-01-10 06:23:32 -0300

    salvando pipeline 02

---
+++ src/pipeline_00.py
@@ -0,0 +1,28 @@
+import requests
+from datetime import datetime
+
+def extrair_dados_bitcoin():
+    """Extrai o JSON completo da API da Coinbase."""
+    url = 'https://api.coinbase.com/v2/prices/spot'
+    resposta = requests.get(url)
+    return resposta.json()
+
+def tratar_dados_bitcoin(dados_json):
+    """Transforma os dados brutos da API e adiciona timestamp."""
+    valor = float(dados_json['data']['amount'])
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+    dados_tratados = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda,
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                }
+    return dados_tratados
+
+
+if __name__ == "__main__":
+    # Extração dos dados
+    dados_json = extrair_dados_bitcoin()
+    dados_tratados = tratar_dados_bitcoin(dados_json)
+    print(dados_tratados)
